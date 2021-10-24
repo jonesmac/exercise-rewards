01d31828f283d1ac9e46f841bfc000a6d156a6f4
Matt Jones <885417+jonesmac@users.noreply.github.com>
2021-10-24 17:21:26 -0400

    remove commented code

--- contracts/ExerciseRewards.sol
+++ contracts/ExerciseRewards.sol
@@ -3,21 +3,6 @@ pragma solidity ^0.8.0;
 
 import 'hardhat/console.sol';
 
-// interface IERC20 {
-
-//     function totalSupply() external view returns (uint256);
-//     function balanceOf(address account) external view returns (uint256);
-//     function allowance(address owner, address spender) external view returns (uint256);
-
-//     function transfer(address recipient, uint256 amount) external returns (bool);
-//     function approve(address spender, uint256 amount) external returns (bool);
-//     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-
-
-//     event Transfer(address indexed from, address indexed to, uint256 value);
-//     event Approval(address indexed owner, address indexed spender, uint256 value);
-// }
-
 contract ExerciseRewards {
     string public constant name = "ExerciseRewards";
     string public constant symbol = "ERWD";
@@ -31,9 +16,6 @@ contract ExerciseRewards {
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
     event Transfer(address indexed from, address indexed to, uint amount);
     event RewardStatus(bool indexed completeStatus, address indexed contractAddress,  uint256 indexed balance);
-
-    // using SafeMath for uint256;
-
 
     constructor(address _exerciser, address _approver, uint256 _payoutIncrement) payable {
         require(msg.value > 0, "msg.value must be greater than 0");
@@ -58,17 +40,3 @@ contract ExerciseRewards {
         }
     }
 }
-
-// library SafeMath {
-//     function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-//       assert(b <= a);
-//       return a - b;
-//     }
-
-//     function add(uint256 a, uint256 b) internal pure returns (uint256) {
-//       uint256 c = a + b;
-//       assert(c >= a);
-//       return c;
-//     }
-// }
-
