11b82c15b4a86159d22c271bb8d433a9f860be89
Matt Jones <885417+jonesmac@users.noreply.github.com>
2021-10-24 21:35:05 -0400

    harden contract code and add more tests

--- contracts/ExerciseRewards.sol
+++ contracts/ExerciseRewards.sol
@@ -5,28 +5,31 @@ import 'hardhat/console.sol';
 
 contract ExerciseRewards {
     string public constant name = "ExerciseRewards";
-    string public constant symbol = "ERWD";
-    uint8 public constant decimals = 18;
 
     address payable private exerciser;
     address private approver;
     uint256 private payoutIncrement;
     uint256 public reward;
 
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
     event Transfer(address indexed from, address indexed to, uint amount);
     event RewardStatus(bool indexed completeStatus, address indexed contractAddress,  uint256 indexed balance);
 
-    constructor(address _exerciser, address _approver, uint256 _payoutIncrement) payable {
+    function setupContract(address _exerciser, address _approver, uint256 _payoutIncrement) public payable returns (bool) {
+        require(exerciser == address(0), "exerciser is already set");
+        require(approver == address(0), "approver is already set");
+        require(_exerciser != _approver, "Exerciser cannot also be the approver");
         require(msg.value > 0, "msg.value must be greater than 0");
+        require(_payoutIncrement > 0, "_payoutIncrement must be greater than 0");
         exerciser = payable(_exerciser);
         approver = _approver;
-        payoutIncrement = _payoutIncrement;
         reward = msg.value;
+        payoutIncrement = _payoutIncrement;
+        return true;
     }
 
     function payout() public returns (uint256) {
         require(msg.sender == approver, "sender is not approver");
+        require(reward > 0, "cannot payout because there is no reward available");
         if (reward > payoutIncrement) {
             exerciser.transfer(payoutIncrement);
             reward = reward - payoutIncrement;
@@ -34,9 +37,16 @@ contract ExerciseRewards {
             return (reward);
         } else {
             exerciser.transfer(reward);
+            reward = 0;
             emit Transfer(address(this), exerciser, address(this).balance);
             emit RewardStatus(true, address(this), address(this).balance);
             return (reward);
         }
+    }
+
+    function deposit() public payable returns (uint256) {
+        require(msg.value > 0, "deposits must be greater than 0");
+        reward = reward + msg.value;
+        return reward;
     }
 }
--- test/index.ts
+++ test/index.ts
@@ -1,46 +1,151 @@
-import { Wallet } from "@ethersproject/wallet";
 import { expect } from "chai";
-import { MockProvider } from "ethereum-waffle";
 import { ethers, waffle } from "hardhat";
+import { ExerciseRewards } from "../typechain";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
-
-    expect(await greeter.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
-});
-
-describe.only('ExerciseRewards', () => {
-  const payoutIncrement =  ethers.utils.parseUnits('2', 'gwei');
+describe('ExerciseRewards', () => {
+  const payoutIncrement = ethers.utils.parseUnits('2', 'gwei');
   const initialContractValue = ethers.utils.parseUnits('10', 'gwei');
   const [approver, exerciser] = waffle.provider.getWallets();
-  
+  let ExerciseRewards;
+  let exerciseRewards: ExerciseRewards;
+
+  beforeEach(async () => {
+    ExerciseRewards = await ethers.getContractFactory("ExerciseRewards");
+    exerciseRewards = await ExerciseRewards.deploy();
+  })
+
+  it('should setup the contract', async () => {
+    const setupContractTx = await exerciseRewards.setupContract(
+      exerciser.address,
+      approver.address,
+      payoutIncrement,
+      { value: initialContractValue }
+    );
+    await setupContractTx.wait();
+    expect(await exerciseRewards.reward()).to.eq(initialContractValue);
+  });
+
   it('should payout when approver says so', async () => {
-    const ExerciseRewards = await ethers.getContractFactory("ExerciseRewards");
-    const exerciseRewards = await ExerciseRewards.deploy(
+    const exerciserInitialBalance = await ethers.provider.getBalance(exerciser.address);
+    const setupContractTx = await exerciseRewards.setupContract(
+      exerciser.address,
+      approver.address,
+      payoutIncrement,
+      { value: initialContractValue }
+    );
+    await setupContractTx.wait();
+
+    expect(await exerciseRewards.reward()).to.eq(initialContractValue);
+
+    const payoutTx = await exerciseRewards.connect(approver).payout();
+    await payoutTx.wait();
+
+    expect(await exerciseRewards.reward()).to.eq(8000000000);
+    
+    const exerciserNewBalance = await ethers.provider.getBalance(exerciser.address)
+    expect(exerciserNewBalance).to.eq(exerciserInitialBalance.add(payoutIncrement));
+  });
+
+  it('should not payout when there is no reward', async () => {
+    const setupContractTx = await exerciseRewards.setupContract(
+      exerciser.address,
+      approver.address,
+      initialContractValue,
+      { value: initialContractValue }
+    );
+    await setupContractTx.wait();
+
+    const payoutTx = await exerciseRewards.connect(approver).payout();
+
+    await payoutTx.wait();
+
+    expect(await exerciseRewards.reward()).to.eq(0);
+  });
+
+  it('should allow for deposits to increase the reward', async () => {
+    const deposit = ethers.utils.parseUnits('2', 'gwei');
+
+    const setupContractTx = await exerciseRewards.setupContract(
       exerciser.address,
       approver.address,
       payoutIncrement,
       { value: initialContractValue }
     );
 
-    expect(await exerciseRewards.reward()).to.eq(initialContractValue);
+    await setupContractTx.wait();
 
-    const payoutTx = await exerciseRewards.connect(approver).payout();
+    const depositTx = await exerciseRewards.deposit({ value: deposit });
 
-    // wait until the transaction is mined
-    await payoutTx.wait();
+    await depositTx.wait();
 
-    expect(await exerciseRewards.reward()).to.eq(8000000000);
+    expect(await exerciseRewards.reward()).to.eq(12000000000);
   })
-})
+
+  it('should fail to set up contract when exerciser is also approver', async () => {
+    try {
+      const setupContractTx = await exerciseRewards.setupContract(
+        exerciser.address,
+        exerciser.address,
+        payoutIncrement,
+        { value: initialContractValue }
+      );
+      await setupContractTx.wait();
+      expect(true).to.eq(false);
+    } catch (e) {
+      expect((e as Error).toString()).to.contains('Exerciser cannot also be the approver');
+    }
+  });
+
+  it('should fail to set up contract when exerciser is already set', async () => {
+    try {
+      const setupContractTx = await exerciseRewards.setupContract(
+        exerciser.address,
+        approver.address,
+        payoutIncrement,
+        { value: initialContractValue }
+      );
+      await setupContractTx.wait();
+
+      // Exerciser already set
+      await exerciseRewards.setupContract(
+        exerciser.address,
+        approver.address,
+        payoutIncrement,
+        { value: initialContractValue }
+      );
+      expect(true).to.eq(false);
+    } catch (e) {
+      expect((e as Error).toString()).to.contains('exerciser is already set');
+    }
+  });
+
+  it('should fail to set up contract when reward is less than 1', async () => {
+    try {
+      const setupContractTx = await exerciseRewards.setupContract(
+        exerciser.address,
+        approver.address,
+        payoutIncrement,
+        { value: 0 }
+      );
+      await setupContractTx.wait();
+
+    } catch (e) {
+      expect((e as Error).toString()).to.contains('msg.value must be greater than 0');
+    }
+  });
+
+  it('should fail to set up contract when _payoutIncrement is less than 1', async () => {
+    try {
+      const setupContractTx = await exerciseRewards.setupContract(
+        exerciser.address,
+        approver.address,
+        0,
+        { value: initialContractValue }
+      );
+      await setupContractTx.wait();
+
+    } catch (e) {
+      expect((e as Error).toString()).to.contains('_payoutIncrement must be greater than 0');
+    }
+  });
+});
