8186ee7ade3077d46464712e7ede41362d38be5a
Matt Jones <885417+jonesmac@users.noreply.github.com>
2021-10-26 21:32:40 -0400

    add stub fields to form

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,23 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import { useMetamask } from 'use-metamask';
+import { ethers } from "ethers";
 import './App.css';
 
-function App() {
+const App = () => {
+  const { connect, metaState } = useMetamask();
+
+  // instead of calling it from useEffect, you can also call connect method from button click handler
+  useEffect(() => {
+    if (!metaState.isConnected) {
+      (async () => {
+        try {
+          await connect(ethers.providers.Web3Provider);
+          console.log('Connection Accepted');
+          console.log(metaState);
+        } catch (error) {
+          console.log(error);
+        }
+      })();
+    }
+  }, [connect, metaState.isConnected, metaState]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Exercise Rewards</h1>
+        <form id="setup_contract">
+          <div className="form-group">
+            <p>
+              <label htmlFor="Sender Address">Reward Total (in ETH)</label><br />
+              <input width="100%" type="string" className="form-control" id="fromAddress" aria-describedby="fromAddressHelp"
+                placeholder="Enter your amount" required={true} />
+            </p>
+            <p>
+              <label htmlFor="Exerciser Address">Exerciser Address</label><br />
+              <input width="100%" value="" type="string" className="form-control" id="exerciserAddress" aria-describedby="fromAddressHelp"
+                placeholder="Enter your amount" required={true} />
+            </p>
+            <p>
+              <label htmlFor="Approver Address">Approver Address</label><br />
+              <input width="100%" value="" type="string" className="form-control" id="approverAddress" aria-describedby="fromAddressHelp"
+                placeholder="Enter your amount" required={true} />
+            </p>
+            <p>
+              <label htmlFor="Payout Amount">Payout Amount (in ETH)</label><br />
+              <input width="100%" value="" type="string" className="form-control" id="payoutAmount" aria-describedby="fromAddressHelp"
+                placeholder="Enter your amount" required={true} />
+            </p>
+          </div>
+        </form>
       </header>
     </div>
   );
